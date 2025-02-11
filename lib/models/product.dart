7b05516c14d770113659e76e30d9fe23be9b4dd0
ownhajaya <hajayaown@gmail.com>
2025-02-11 14:56:29 +0300

    all tasks

---
+++ lib/models/product.dart
@@ -0,0 +1,43 @@
+// Product Model (json to dart)
+class Product {
+  int? id;
+  String? title;
+  double? price;
+  String? description;
+  String? category;
+  String? image;
+  Rating? rating;
+
+  Product({
+    this.id,
+    this.title,
+    this.price,
+    this.description,
+    this.category,
+    this.image,
+    this.rating,
+  });
+
+  Product.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    price = (json['price'] as num).toDouble(); // Ensure it's double
+    description = json['description'];
+    category = json['category'];
+    image = json['image'];
+    rating = json['rating'] != null ? Rating.fromJson(json['rating']) : null;
+  }
+}
+
+// Rating Model
+class Rating {
+  double? rate;
+  int? count;
+
+  Rating({this.rate, this.count});
+
+  Rating.fromJson(Map<String, dynamic> json) {
+    rate = (json['rate'] as num).toDouble(); // Ensure it's double
+    count = json['count'];
+  }
+}
